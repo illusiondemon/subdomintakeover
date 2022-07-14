c350cd1ff447e474f35f003e1e527d5a3f29e00e
Nahid Alam <88379378+illusiondemon@users.noreply.github.com>
2022-07-14 06:54:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-#DO NOT COPY
-<table width=100% height=100%><td align=center><font face="sans-serif" size="50" color="grey"><strong><i><b>SerVeR P</i>wN<i>e</i>D</i></strong></font><br><font face="sans-serif" size="5" color="grey">by</font><DIV class=cont><A href=""><span style='font: ;color:darkred; text-shadow: 0px 0px 10px;'>D3AD Cyber Army</span></div><br><style>body{overflow:hidden;background-color:black}#q{font:40px impact;color:white;position:absolute;left:0;right:0;top:43%}.cont a{  text-decoration: none;  font-family: sans-serif;  font-size:750%;}}</style>
-<table width=100% height=100%><td align=center><font face="sans-serif" size="50" color="grey"><strong><i><b>Respect Holy Prophet Muhammad (S.A.W.W)</i></strong></font><br><font face="sans-serif" size="5" color="grey">-------------------------------------------------------------------------------------------------------------------</font><table width=100% height=100%><td align=center><font face="sans-serif" size="5" color="grey"><strong><i><b>"Always Be A Leader" "Never Be a Follower"</i></strong></font><br>font></strong></font><br><font face="sans-serif" size="5" color="grey">"We" "Are" "NEVER" "Give" "up"</font><table width=100% height=100%><td align=center><font face="sans-serif" size="5" color="grey"><strong><i><b>#D3ADCyberArmy #YourDeadArmy #D3ADArmy</i></strong></font><br>
-	<style type="text/css">body, a, a:hover {cursor: url(http://cur.cursors-4u.net/cursors/cur-11/cur1054.cur), progress;}</style>
-<style type="text/css">
-body{
-background:url(https://lh3.googleusercontent.com/-CYobas4WGXo/Ts32QusPKsI/AAAAAAAAAgw/xQQMNbgAa0M/s800/port_listener.gif) repeat center center fixed black;
-}
-</style>
-<title>H@cKeD By D3AD Cyber Army</title>
-</script>
-<audio autoplay>
-<source src="https://b.top4top.io/m_2365b29d20.mp3" type="audio/mpeg">
-</audio>
+Takeover By Nahid Alam
